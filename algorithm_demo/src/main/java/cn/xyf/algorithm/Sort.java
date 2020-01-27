181d62f0de4d30e219d0fa05528f6afd2e7a4c45
linkcheng <zheng.long@shoufuyou.com>
2020-01-27 14:23:47 +0800

    merge sort

--- algorithm_demo/src/main/java/cn/xyf/algorithm/Sort.java
+++ algorithm_demo/src/main/java/cn/xyf/algorithm/Sort.java
@@ -1,6 +1,8 @@
 package cn.xyf.algorithm;
 
 
+import java.util.Arrays;
+
 /*升序*/
 public class Sort {
     public void bubbleSort(int[] arr) {
@@ -125,6 +127,84 @@ public class Sort {
     }
 
     /**
+     * 求小和问题
+     * 求左边数小于当前数的累计和
+     * 4 1 3 5     0 6
+     * 0 0 1 4+1+3 0 4+1+3+5  =  22
+     * 转化为：右边数比当前数大的数量*当前数
+     */
+    public int sumByMergeSort(int[] arr) {
+        if(arr==null || arr.length<2) {
+            return 0;
+        }
+        return msSortProcessForSum(arr, 0, arr.length-1);
+    }
+
+    /**
+     * 归并排序的排序过程
+     * @param arr 要排序的数组
+     * @param left 数组中待排序部分的左边位置
+     * @param right 数组中待排序部分的右边位置
+     */
+    private int msSortProcessForSum(int[] arr, int left, int right) {
+        if(left==right) {
+            return 0;
+        }
+        // 中点
+        int mid = left + ((right-left)>>1);
+
+        // 左边排序
+        int sumLeft = msSortProcessForSum(arr, left, mid);
+
+        // 右边排序
+        int sumRight = msSortProcessForSum(arr, mid+1, right);
+
+        // 合并
+        int sumMerge = msMergeProcessForSum(arr, left, mid, right);
+        return sumMerge + sumLeft + sumRight;
+    }
+
+    /**
+     * 求小和问题
+     * 求左边数小于当前数的累计和
+     * 4 1 3 5     0 6
+     * 0 0 1 4+1+3 0 4+1+3+5  =  22
+     */
+    private int msMergeProcessForSum(int[] arr, int left, int mid, int right) {
+        int[] help = new int[right - left + 1];
+        int i = 0;
+        int p1 = left;
+        int p2 = mid + 1;
+        int sum = 0;
+
+        // 从小到大依次从两个数组中选择数据合并
+        while(p1<=mid && p2<=right) {
+            // 发现一个右边大于左边的数字，记录一次
+            if(arr[p1] < arr[p2]) {
+                sum += arr[p1]*(right-p2+1);
+            }
+//            sum +=  arr[p1] < arr[p2] ?  arr[p1]*(right-p2+1) : 0;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+
+        // 两个有且只有一个越界
+        while(p1<=mid) {
+            help[i++] = arr[p1++];
+        }
+
+        while(p2<=right) {
+            help[i++] = arr[p2++];
+        }
+
+        // 覆盖原数组
+        for(i=0; i<help.length; i++) {
+            arr[left+i] = help[i];
+        }
+
+        return sum;
+    }
+
+    /*
      * 贪心策略：脑补一个标准，按照这个标准确定一个答案
      * 比如：时间轴上有如多课程，按照课程最早结束，可以安排出一天最多的课程数量
      */
--- algorithm_demo/src/test/java/cn/xyf/algorithm/SortTest.java
+++ algorithm_demo/src/test/java/cn/xyf/algorithm/SortTest.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 @Slf4j
 public class SortTest {
@@ -48,4 +51,69 @@ public class SortTest {
         sort.mergeSort(arr);
         log.info(Arrays.toString(arr));
     }
+
+    @Test
+    public void testSumByMergeSort() {
+        int[] arr = {4,1,3,5,0,6};
+        log.info(Arrays.toString(arr));
+        int sum = sort.sumByMergeSort(arr);
+        log.info("sum="+sum);
+        log.info(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testReflect() throws Exception{
+        printClassInfo("".getClass());
+        printClassInfo(Runnable.class);
+        printClassInfo(java.time.Month.class);
+        printClassInfo(String[].class);
+        printClassInfo(int.class);
+        printClassInfo(int[].class);
+//        printMethodInfo(int.class);
+        printMethodInfo(String.class);
+    }
+
+    public void printClassInfo(Class cls) {
+        log.info("=================");
+        log.info("Class name: " + cls.getName());
+        log.info("Simple name: " + cls.getSimpleName());
+        if (cls.getPackage() != null) {
+            log.info("Package name: " + cls.getPackage().getName());
+        }
+        log.info("is interface: " + cls.isInterface());
+        log.info("is enum: " + cls.isEnum());
+        log.info("is array: " + cls.isArray());
+        log.info("is primitive: " + cls.isPrimitive());
+    }
+
+    public void printMethodInfo(Class cls) throws Exception{
+        log.info("=================");
+//        log.info(Arrays.toString(cls.getMethods()));
+        // 获取public方法getScore，参数为String:
+        log.info(cls.getMethod("indexOf", int.class)+"");
+        // 获取继承的public方法getName，无参数:
+        log.info(cls.getMethod("toString")+"");
+        Method indexOf = cls.getMethod("indexOf", String.class);
+        log.info(indexOf.invoke("hello22", "2")+"");
+
+    }
+
+
+    @Test
+    public void testList() {
+        List<String> list = Arrays.asList("apple", "pear", "banana");
+        list.toArray();
+
+        for(Iterator<String> iterator=list.iterator(); iterator.hasNext(); ) {
+            log.info(iterator.next());
+        }
+
+        for(String string: list){
+            log.info(string);
+        }
+
+        String s = "c";
+        log.info(s.equals(new String("c"))+"");
+    }
+
 }
