6b50ddccdcc6860e827ce8af7f2f1e5ea0f6839f
linkcheng <zheng.long@shoufuyou.com>
2020-02-23 16:33:55 +0800

    spring mvc xml

--- springMVC/src/main/java/cn/xyf/controller/HelloController.java
+++ springMVC/src/main/java/cn/xyf/controller/HelloController.java
@@ -9,14 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloController implements Controller {
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        //ModelAndView 模型和视图
+        // ModelAndView 模型和视图
         ModelAndView mv = new ModelAndView();
 
-        //封装对象，放在ModelAndView中。Model
+        // 封装对象，放在ModelAndView中。Model
         mv.addObject("msg","HelloSpringMVC!");
 
-        //封装要跳转的视图，放在ModelAndView中
-        mv.setViewName("hello"); //: /WEB-INF/jsp/hello.jsp
+        // 封装要跳转的视图，放在ModelAndView中
+        // /WEB-INF/jsp/hello.jsp
+        mv.setViewName("hello");
 
         return mv;
     }
