d8f601776a62ed927af1159260fb5aac5191d68e
linkcheng <zheng.long@shoufuyou.com>
2020-03-20 10:45:17 +0800

    reflect

--- juc-demo/src/main/java/cn/xyf/JucDemoApplication.java
+++ juc-demo/src/main/java/cn/xyf/JucDemoApplication.java
@@ -1,6 +1,7 @@
 package cn.xyf;
 
-import cn.xyf.config.ConcurrentExecutorConfig;
+import cn.xyf.config.Column;
+import cn.xyf.config.Table;
 import cn.xyf.pojo.Blog;
 import cn.xyf.pojo.StarUser;
 import cn.xyf.service.JucBlogServiceImpl;
@@ -12,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.List;
 
 
@@ -19,9 +22,31 @@ import java.util.List;
 @MapperScan("cn.xyf.mapper")
 public class JucDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         ConfigurableApplicationContext context = SpringApplication.run(JucDemoApplication.class, args);
 
+        test(context);
+
+    }
+
+    public static void test2() throws ClassNotFoundException {
+
+        Class cls = Class.forName("cn.xyf.pojo.Student");
+
+        Table table = (Table) cls.getAnnotation(Table.class);
+        System.out.println("table.name="+table.name());
+
+
+        Field[] fields = cls.getDeclaredFields();
+        for (Field field : fields) {
+            Column col = field.getAnnotation(Column.class);
+            System.out.println("col.name="+col.name()
+                    +",col.type="+col.type()
+                    +",col.length="+col.length());
+        }
+    }
+
+    public static void test(ConfigurableApplicationContext context) {
         ThreadPoolTaskExecutor executor = context.getBean("taskConcurrentExecutor", ThreadPoolTaskExecutor.class);
         System.out.println(executor.getThreadNamePrefix());
 
--- juc-demo/src/main/java/cn/xyf/service/StarUserServiceImpl.java
+++ juc-demo/src/main/java/cn/xyf/service/StarUserServiceImpl.java
@@ -2,12 +2,14 @@ package cn.xyf.service;
 
 import cn.xyf.mapper.StarUserMapper;
 import cn.xyf.pojo.StarUser;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -27,5 +29,22 @@ public class StarUserServiceImpl implements StarUserService {
              users.add(mapper.select(i));
         }
         return users;
+    }
+
+    private <T> T parseJsonObject(JSONObject params, String key, Class<T> cls) {
+        if (!params.containsKey(key)) {
+            System.out.println("key " + key + "not exists");
+        }
+        return params.getObject(key, cls);
+    }
+
+    private JSONObject updateContext(JSONObject context, List<String> eleIds, Map<String, Object> elements) {
+        if(null != elements){
+            context.putAll(elements);
+        } else {
+            //如果接口返回空就把每个因子填到context中值为null
+            eleIds.forEach(eleId -> {if(!context.containsKey(eleId)) context.put(eleId, null);});
+        }
+        return context;
     }
 }
