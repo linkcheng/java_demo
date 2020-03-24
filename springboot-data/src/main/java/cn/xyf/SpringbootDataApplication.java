2c63c7a28e0e3949bcb736ea872b90d2699a42a2
linkcheng <zheng.long@shoufuyou.com>
2020-03-24 15:41:23 +0800

    add spring data redis

--- springboot-data/src/main/java/cn/xyf/SpringbootDataApplication.java
+++ springboot-data/src/main/java/cn/xyf/SpringbootDataApplication.java
@@ -3,6 +3,8 @@ package cn.xyf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class SpringbootDataApplication {
 
