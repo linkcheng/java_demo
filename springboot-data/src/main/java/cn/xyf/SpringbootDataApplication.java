3251e7527b06e7a96603d28097a0a01e473fba40
linkcheng <zhenglong1992@126.com>
2020-03-24 18:42:57 +0800

    add mongo dependency

--- springboot-data/src/main/java/cn/xyf/SpringbootDataApplication.java
+++ springboot-data/src/main/java/cn/xyf/SpringbootDataApplication.java
@@ -3,8 +3,6 @@ package cn.xyf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class SpringbootDataApplication {
 
